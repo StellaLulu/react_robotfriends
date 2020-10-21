d199e1c3f138d9f7051577ce6a9a62a46bf25a73
StellaLulu <cty0220@gmail.com>
2020-10-21 23:58:05 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import CardList from './CardList';
+//import {robots} from './robots';
+import SearchBox from './SearchBox';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component{
+    constructor(){
+        super() // To call the parent constructor (Component)
+        this.state = {
+                robots: [],
+                searchfield: ''
+            }
+            //console.log('constructor')
+        }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({robots: users}));
+        
+        //this.setState({ robots: robots});
+        //console.log('componentDidMount')
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        // console.log(event.target.value); // event must have target.value to return input text in console
+    }
+    
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        //console.log('render');
+        if (this.state.robots.length === 0){   // if the fetch take a long time to read the users
+            return (
+            <div className='tc'>
+                <h1>Loading</h1>
+            </div>
+            )
+        }
+        else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
+
+// Console runs with constructor > render > componentDidMount > render
+// Q: why it renders again? because in componenetDidMount, the empty array get the robots so it renders again
