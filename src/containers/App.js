c16f5adec0c5217ac996665381c8da7c7bcff443
StellaLulu <cty0220@gmail.com>
2020-10-25 16:51:41 +0800

    added scroll component and build folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
+import CardList from '../components/CardList';
 //import {robots} from './robots';
-import SearchBox from './SearchBox';
+import SearchBox from '../components//SearchBox';
+import Scroll from '../components//Scroll';
 import './App.css';
 
 
@@ -31,28 +32,28 @@ class App extends Component{
     }
     
     render(){
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const{robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         //console.log('render');
-        if (this.state.robots.length === 0){   // if the fetch take a long time to read the users
-            return (
+        return !robots.length?   // if the fetch take a long time to read the users, if (robots.length === 0)(
+        (
             <div className='tc'>
                 <h1>Loading</h1>
             </div>
-            )
-        }
-        else{
-            return(
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+        ):
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
                     <CardList robots={filteredRobots}/>
-                </div>
-            );
+                </Scroll>
+            </div>
+        );
         }
     }
-}
 
 export default App;
 
